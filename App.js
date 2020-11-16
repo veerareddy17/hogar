2b76faad3411be379731490d5f72b3f31a394578
veerareddy <gottamvreddy@visionplanner.com>
2020-11-16 11:41:52 +0530

    progress value text color changed

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, Text, Animated, StyleSheet, Easing } from 'react-native';
-import AnimatedProgressWheel from 'react-native-progress-wheel';
 import CircularSlider from './CircularSlider';
 export default class App extends Component {
   constructor() {
@@ -17,7 +16,7 @@ export default class App extends Component {
         <CircularSlider width={300} height={300} meterColor='#0cd'
               value={this.state.slider} onValueChange={(value)=>this.setState({slider:value})}/>
               <View style={{height: 200, width: 200, position: 'absolute', justifyContent: 'center', alignItems: 'center'}}>
-                 <Text style={{color: 'balck'}}>{`${sliderText}`}</Text>
+                 <Text style={{color: 'black'}}>{`${sliderText}`}</Text>
               </View>
       </View>
 
